0a8a023b8dd22650ed6cb722b261992808fda324
Aeron Roemer <aeronroemer@users.noreply.github.com>
2020-08-02 13:33:18 -0600

    Background Image in Customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -116,13 +116,32 @@ $wp_customize->add_section(
 			'label' => 'Information',
 			'type' => 'textarea'
 		)
-    );
+	);
+//Add main Image Setting
+//Alert Panel
+$wp_customize->add_section(
+    'background-photo', 
+    array(
+    'title' => 'Background Photo',
+	'description' => 'Add or change the backgound image.',
+    'capability' => 'edit_theme_options',
+    'priority' => 32
+    ));
+	
+$wp_customize->add_setting('bg_img');
+$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'bg_img',
+array(
+'label' => 'Upload Image',
+'section' => 'background-photo',
+'settings' => 'bg_img',
+) ) );
+
 // add a setting for the site logo
 $wp_customize->add_setting('your_theme_logo');
 // Add a control to upload the logo
 $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'your_theme_logo',
 array(
-'label' => 'NEw added Upload Logo',
+'label' => 'Upload Logo',
 'section' => 'title_tagline',
 'settings' => 'your_theme_logo',
 ) ) );
