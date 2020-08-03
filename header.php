e9715ecaccc1304f01ada83d56765222a05b5b80
Aeron Roemer <aeronroemer@users.noreply.github.com>
2020-08-03 17:52:09 -0600

    Background Image Positioning and Style added changes to stylesheet as well as inline styles that get the image URL from customizer

--- header.php
+++ header.php
@@ -9,36 +9,39 @@
     </head>
     <body <?php body_class(); ?>>
     <div id="page">
-        <div class="header">
-        <header id="masthead" class="header-container" role="banner">
-            <a href="#content" class="skip-link screen-reader-text" display="none">
-                <?php esc_html_e( 'Skip to content', 'beastro' ); ?> 
-            </a>
-                <div class="site-branding">
-                    <p class="site-description">
-                        <?php bloginfo( 'description' ); ?>
-                    </p>
-                </div>
-                <div class="custom-info">
-                    <div class="header-hours">
-                        <?php echo get_theme_mod('hours'); ?>
+        <div class="nav">
+            <div class="nav-content">
+                <header id="masthead" class="header-container" role="banner">
+                    <a href="#content" class="skip-link screen-reader-text" display="none">
+                        <?php esc_html_e( 'Skip to content', 'beastro' ); ?> 
+                    </a>
+                </header>
+                        <div class="site-branding">
+                            <p class="site-description">
+                                <?php bloginfo( 'description' ); ?>
+                            </p>
+                        </div>
+                        <div class="custom-info">
+                            <div class="header-hours">
+                                <?php echo get_theme_mod('hours'); ?>
+                            </div>
+                            <div class="header-phone">
+                                <?php echo get_theme_mod('phone'); ?>
+                            </div>
+                            <div class="header-location">
+                                <?php echo get_theme_mod('location'); ?>
+                            </div>
+                        </div>
+                        <div class="header-links">
+                            <img src="<?php echo get_template_directory_uri(); ?>/inc/img/menu-link.png" class="header-link"/>
+                            <img src="<?php echo get_template_directory_uri(); ?>/inc/img/order-link.png" class="header-link"/>
+                            <img src="<?php echo get_template_directory_uri(); ?>/inc/img/about-link.png" class="header-link"/>
+                        </div>
+                        <div class="social-icons">
+                            <img src="<?php echo get_template_directory_uri(); ?>/inc/img/insta-icon.png" class="social-icon"/>
+                            <img src="<?php echo get_template_directory_uri(); ?>/inc/img/fb-icon.png" class="social-icon"/>
+                        </div>
                     </div>
-                    <div class="header-phone">
-                        <?php echo get_theme_mod('phone'); ?>
-                    </div>
-                    <div class="header-location">
-                        <?php echo get_theme_mod('location'); ?>
-                    </div>
-                </div>
-                <div class="header-links">
-                    <img src="<?php echo get_template_directory_uri(); ?>/inc/img/menu-link.png" class="header-link"/>
-                    <img src="<?php echo get_template_directory_uri(); ?>/inc/img/fb-icon.png" class="header-link"/>
-                </div>
-                <div class="social-icons">
-                    <img src="<?php echo get_template_directory_uri(); ?>/inc/img/insta-icon.png" class="social-icon"/>
-                    <img src="<?php echo get_template_directory_uri(); ?>/inc/img/fb-icon.png" class="social-icon"/>
-                </div>
-            </header>
-        </div> 
-        <div id="content" class="site-content">
+            </div> 
+            <div id="content" class="site-content">
         
--- template-parts/background.php
+++ template-parts/background.php
@@ -1,7 +1,8 @@
 <?php
 // check to see if the logo exists and add it to the page
 if ( get_theme_mod( 'bg-img' ) ) : ?>
+ <div class="background-image" style="background-image: linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)),
+     url('<?php echo get_theme_mod( 'bg-img' ); ?>'); ">
  
-<img src="<?php echo get_theme_mod( 'bg-img' ); ?>" alt="Background Image" >
- 
+</div>
 <?php endif; ?>
