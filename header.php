a88118196ef233f9b53d95bdd6c331cb95ae1ec0
Aeron Roemer <aeronroemer@users.noreply.github.com>
2020-08-04 14:36:56 -0600

    Header Images & Layout Added the images and logo to the header area, created a semi-responsive layout. Next steps are to connect nav images to actual links, create media queries and add styling for alert section.

--- header.php
+++ header.php
@@ -8,14 +8,20 @@
         <!-- allows links to stylesheets etc -->
     </head>
     <body <?php body_class(); ?>>
+    <?php
+        // check to see if the logo exists and add it to the page
+        if ( get_theme_mod( 'bg-img' ) ) : ?>
+        <div class="background-image" style="background-image: linear-gradient(rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.85)),
+            url('<?php echo get_theme_mod( 'bg-img' ); ?>'); ">
+        </div>
+        <?php endif; ?>
     <div id="page">
-        <div class="nav">
-            <div class="nav-content">
-                <header id="masthead" class="header-container" role="banner">
+            <header id="masthead" class="header-container" role="banner">
                     <a href="#content" class="skip-link screen-reader-text" display="none">
                         <?php esc_html_e( 'Skip to content', 'beastro' ); ?> 
                     </a>
-                </header>
+                <div class="nav">
+                    <div class="nav-content">
                         <div class="site-branding">
                             <p class="site-description">
                                 <?php bloginfo( 'description' ); ?>
@@ -32,10 +38,10 @@
                                 <?php echo get_theme_mod('location'); ?>
                             </div>
                         </div>
-                        <div class="header-links">
-                            <img src="<?php echo get_template_directory_uri(); ?>/inc/img/menu-link.png" class="header-link"/>
-                            <img src="<?php echo get_template_directory_uri(); ?>/inc/img/order-link.png" class="header-link"/>
-                            <img src="<?php echo get_template_directory_uri(); ?>/inc/img/about-link.png" class="header-link"/>
+                        <div class="nav-links">
+                            <img src="<?php echo get_template_directory_uri(); ?>/inc/img/menu-link.png" class="nav-link"/>
+                            <img src="<?php echo get_template_directory_uri(); ?>/inc/img/order-link.png" class="nav-link"/>
+                            <img src="<?php echo get_template_directory_uri(); ?>/inc/img/about-link.png" class="nav-link"/>
                         </div>
                         <div class="social-icons">
                             <img src="<?php echo get_template_directory_uri(); ?>/inc/img/insta-icon.png" class="social-icon"/>
@@ -43,5 +49,15 @@
                         </div>
                     </div>
             </div> 
+            <div class="header-container">
+                <div class="beast-container">
+                    <img src="<?php echo get_template_directory_uri(); ?>/inc/img/beast.png" class="beast"/>
+                </div>
+                <div class="text-header">        
+                    <img src="<?php echo get_template_directory_uri(); ?>/inc/img/mark-of.png" class="mark-of">
+                    <img src="<?php echo get_template_directory_uri(); ?>/inc/img/vegan-diner.png" class="vegan-diner"/>
+                </div>
+                </div>
+        </header>
             <div id="content" class="site-content">
         
--- template-parts/background.php
+++
@@ -1,8 +0,0 @@
-<?php
-// check to see if the logo exists and add it to the page
-if ( get_theme_mod( 'bg-img' ) ) : ?>
- <div class="background-image" style="background-image: linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)),
-     url('<?php echo get_theme_mod( 'bg-img' ); ?>'); ">
- 
-</div>
-<?php endif; ?>
