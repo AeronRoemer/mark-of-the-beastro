d931b6a35a828917ecbc6d0af135524c6a87cd59
Aeron Roemer <aeronroemer@users.noreply.github.com>
2020-08-04 16:16:14 -0600

    Alert Styling Typeface and effect added for alert.

--- template-parts/alert.php
+++ template-parts/alert.php
@@ -1,6 +1,6 @@
 <?php if('' != get_theme_mod('alert-header')): ?>
     <section class="alert">
-        <h1 class='alert-header'><?php echo get_theme_mod('alert-header'); ?></h1>
+        <h1 class='alert-header neon-text'><?php echo get_theme_mod('alert-header'); ?></h1>
         <?php if('' != get_theme_mod('alert-info')): ?>
             <p class="alert-text"><?php echo get_theme_mod('alert-info'); ?></p>
     <?php endif; ?>
